1e2304445818819e8c5b483db10a5e4a065e51d1
Leo <l3oxon3@gmail.com>
2024-01-11 19:01:27 -0300

    Update App.js

--- v.0.2/portifolio/src/App.js
+++ v.0.2/portifolio/src/App.js
@@ -1,22 +1,26 @@
 import './App.css'
 import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from 'framer-motion';
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
+import Initial from './components/Initial';
 
 function App() {
   const location = useLocation()
 
   return (
     <>
-      <Navbar />
-      <Routes location={location} key={location.pathname}>
-        <Route index element={<Home/>} ></Route>
-        <Route path="/about" element={<About />} ></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
+      <AnimatePresence mode='wait'>
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />} ></Route>
+          <Route path='/initial' element={<Initial />}></Route>
+          <Route path="/about" element={<About />} ></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
