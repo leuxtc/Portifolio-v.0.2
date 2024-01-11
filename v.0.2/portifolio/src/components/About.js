b36aec704438f48913010255cb68e443d2c27486
Leo <l3oxon3@gmail.com>
2024-01-11 19:01:25 -0300

    Update About.js

--- v.0.2/portifolio/src/components/About.js
+++ v.0.2/portifolio/src/components/About.js
@@ -1,10 +1,15 @@
+import React from "react"
+import transition from "../transition"
+import styles from "./About.module.css"
+import Navbar from "./Navbar"
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <h2>I'm a programming student. I have completed high school and now I am looking to pursue a career in web development. I have been studying programming on my own for three years and acquired skills in React.js, JavaScript, HTML, CSS, Python and C#. I have already developed some personal and professional projects using these technologies, which can be found on my GitHub profile. My goal is to continue learning and getting to know more and more about this world, and contribute to creating digital solutions that make a difference in people's live</h2>
+        <div className={styles.about}>
+            <Navbar />
+            <h1>About</h1>        
         </div>
     )
 }
 
-export default About
+export default transition(About)
